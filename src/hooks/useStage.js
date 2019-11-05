5be8e4f130715fe13df6eac34315e8cf65317f94
Wallace Saturnino <wallacesat@live.co.uk>
2019-11-05 10:28:27 -0300

    Implementa a exclusão da linha quando preenchida totalmente

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,8 +4,22 @@ import { createStage } from '../matriz';
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const sweepRows = newStage =>
+      newStage.reduce((acc, row) => {
+        if (row.findIndex(cell => cell[0] === 0) === -1) {
+          setRowsCleared(prev => prev + 1);
+          acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+          return acc;
+        }
+        acc.push(row);
+        return acc;
+      }, []);
+
     const updateStage = prevStage => {
       const newStage = prevStage.map(row =>
         row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
@@ -24,6 +38,7 @@ export const useStage = (player, resetPlayer) => {
 
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
 
       return newStage;
@@ -32,5 +47,5 @@ export const useStage = (player, resetPlayer) => {
     setStage(prev => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
